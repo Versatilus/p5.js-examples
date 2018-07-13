624147780d7be2efbd6b8e346ceff3308cda8622
Eric Paulson <Paulson.Eric@Gmail.com>
2018-07-13 15:29:13 -0700

    Generalized formulas for generic "metallic" ratios.

--- phyllotaxis/sketch.js
+++ phyllotaxis/sketch.js
@@ -16,9 +16,16 @@
     animator, equilateral_triangle, equilateral_triangle2, cosineColors */
 
 var petals = [];
-const goldenRatio = (1 + Math.sqrt(5)) / 2;
-const goldenAngle = 1 / (goldenRatio * goldenRatio) * 2 * Math.PI;
-const gcr = 1 / (goldenRatio * goldenRatio);
+let metallicRatio;
+let metallicAngle;
+let mcr;
+const generateMetallic = n => {
+  console.log(n);
+  metallicRatio = (n + Math.sqrt(n * n + 4)) / 2;
+  mcr = 1 / (metallicRatio * metallicRatio);
+  metallicAngle = mcr * 2 * Math.PI;
+};
+// generateMetallic(1);
 var petalShapes = [];
 var petalShapeChoices = [];
 var animating = false;
@@ -33,24 +40,27 @@ let colorScaler,
   colorWidth,
   backgroundTransparency,
   lineWeight,
-  petalTransparency;
+  petalTransparency,
+  metallicNumber;
 
 optionParser();
 
 growthFactor = setDefault(growthFactor, 15);
-petalRadius = setDefault(petalRadius, goldenRatio * growthFactor);
+petalRadius = setDefault(petalRadius, metallicRatio * growthFactor);
 spiralCount = setDefault(spiralCount, 2);
 rotationDirection = setDefault(rotationDirection, 1); // 1 == clockwise, -1 == counterclockwise
 colorWidth = setDefault(colorWidth, 7); // Higher is narrower.
 colorScaler = setDefault(colorScaler, Math.asin(0.5)); // Starting hue.
 backgroundTransparency = setDefault(backgroundTransparency, 0);
 lineWeight = setDefault(lineWeight, 1);
+metallicNumber = setDefault(metallicNumber, 1);
 petalTransparency = setDefault(petalTransparency, 0);
+generateMetallic(metallicNumber);
 
 let bgColor = [0, 0, 0, 1 - backgroundTransparency];
 var animator = (deflt = 0) => (animating ? frameCount / animationRate : deflt);
 var goldenFilter = (breadth, magnitude, spirals) =>
-  goldenAngle / (breadth + gcr * magnitude + (gcr * spirals) % 1);
+  metallicAngle / (breadth + mcr * magnitude + (mcr * spirals) % 1);
 
 const canvasSize = () => [windowWidth, windowHeight];
 
@@ -75,11 +85,11 @@ function setup() {
           [equilateral_triangle2, 1, PI / 6],
           [equilateral_triangle2, 1, 0],
           [equilateral_triangle2, 2, 0],
-          [ellipse, 1, 1 / goldenRatio],
-          [ellipse, 1 / goldenRatio, 1],
+          [ellipse, 1, 1 / metallicRatio],
+          [ellipse, 1 / metallicRatio, 1],
           [rect, 2, 2],
-          [rect, 2 / goldenRatio, 2],
-          [rect, 2, 2 / goldenRatio]
+          [rect, 2 / metallicRatio, 2],
+          [rect, 2, 2 / metallicRatio]
         ][index]
       );
     }
@@ -125,14 +135,14 @@ function equilateral_triangle2(cx, cy, s, rotation = 0) {
 //   } = parameters;
 //   return phi / (Math.PI * 2);
 // };
-var rotator = ({ rotationAngle: phi }) =>
+var rotator = ({rotationAngle: phi}) =>
   (-rotationDirection * phi) %
-  goldenFilter(1, 0.005 * goldenRatio, spiralCount);
+  goldenFilter(1, 0.005 * metallicRatio, spiralCount);
 
 // var rotator = ({rotationAngle})=>rotationAngle;
-//1 + (gcr * -0.005 + gcr * 1 % 1)
+//1 + (mcr * -0.005 + mcr * 1 % 1)
 
-var functionColors = (mathFunction = Math.sin) => ({ rotationAngle: phi }) =>
+var functionColors = (mathFunction = Math.sin) => ({rotationAngle: phi}) =>
   abs(
     mathFunction(
       phi % goldenFilter(colorWidth, rotationDirection * 0.005, spiralCount) +
@@ -166,7 +176,7 @@ var transparency = () => 1 - petalTransparency;
 
 function Petal(options) {
   this.parameters = options;
-  let { x: x, y: y, petalRadius: radius } = options;
+  let {x: x, y: y, petalRadius: radius} = options;
   this.x = x;
   this.y = y;
   this.radius = radius;
@@ -204,7 +214,7 @@ function createSpiral(gf = growthFactor, radius = petalRadius) {
       sq(sqrt(sq(canvas.width / 2) + sq(canvas.height / 2)) / gf + radius) + 1
     );
   for (let n = 0; n < as; ++n) {
-    var phi = n * goldenAngle,
+    var phi = n * metallicAngle,
       r = gf * Math.sqrt(n),
       x = Math.cos(phi) * r,
       y = Math.sin(phi) * r;
@@ -221,7 +231,7 @@ function createSpiral(gf = growthFactor, radius = petalRadius) {
     );
     // arr[n] = new Petal(x, y, radius, gf, n, phi);
     // arr[n] = new Petal(x, y, radius,
-    //   abs(Math.sin(phi % (goldenAngle / colorFilter) + colorScaler)),
+    //   abs(Math.sin(phi % (metallicAngle / colorFilter) + colorScaler)),
     //   phi);
   }
   petals = arr;
@@ -235,7 +245,7 @@ function updater() {
     petal.show();
   }
   // if (animating && frameCount % 120 === 0)
-  //   console.log((1 + (gcr * 0.005 * (animator(5) % 10) + gcr * 1 % 1)));
+  //   console.log((1 + (mcr * 0.005 * (animator(5) % 10) + mcr * 1 % 1)));
 }
 
 function draw() {
@@ -252,38 +262,41 @@ function windowResized() {
 function optionParser() {
   let parameters = new URLSearchParams(window.location.search);
   petalShapeChoices = Array.from(
-    parameters.getAll('shape'),
+    parameters.getAll("shape"),
     x => (isFinite(x) ? Math.max(Math.min(x, 9), 0) : 0)
   );
   for (let setting of parameters.keys()) {
     let scratch = Number(parameters.get(setting));
     switch (setting) {
-      case 'spirals':
+      case "spirals":
         spiralCount = ~~scratch;
         break;
-      case 'distance_apart':
+      case "distance_apart":
         growthFactor = Math.max(scratch, 1);
         break;
-      case 'petal_radius':
+      case "petal_radius":
         petalRadius = Math.max(scratch, 1);
         break;
-      case 'color_start':
+      case "color_start":
         colorScaler = Math.max(Math.min(scratch, 1), 0);
         break;
-      case 'color_thinness':
+      case "color_thinness":
         colorWidth = Math.round(scratch);
         break;
-      case 'magnitude':
+      case "magnitude":
         rotationDirection = scratch;
         break;
-      case 'line_thickness':
+      case "line_thickness":
         lineWeight = Math.max(Math.min(scratch, 5), 0);
         break;
-      case 'petal_transparency':
+      case "petal_transparency":
         petalTransparency = Math.max(Math.min(scratch, 1), 0);
         break;
-      case 'background_transparency':
+      case "background_transparency":
         backgroundTransparency = Math.max(Math.min(scratch, 1), 0);
+        break;
+      case "metallic_number":
+        metallicNumber = (scratch);
         break;
     }
   }
